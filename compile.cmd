8c615c09006bfa845a0d9455c918a693e186546c
Carlos Montiers Aguilera <cmontiers@gmail.com>
2019-08-07 23:40:25 -0400

    Add version resource file

--- compile.cmd
+++ compile.cmd
@@ -1,5 +1,9 @@
 @Echo Off
 
-gcc -Wl,-e,__dllstart,--enable-stdcall-fixup -nostartfiles -O3 -s -m32 -shared dll_enhancedbatch.c extensions.c offsets.c -o enhancedbatch_x86.dll -Wall
+windres -F pe-i386 enhancedbatch.rc enhancedbatch_info_x86.o
+gcc -Wl,-e,__dllstart,--enable-stdcall-fixup -nostartfiles -O3 -s -m32 -shared dll_enhancedbatch.c extensions.c offsets.c enhancedbatch_info_x86.o -o enhancedbatch_x86.dll -Wall
 
-gcc -Wl,-e,_dllstart -nostartfiles -O3 -s -m64 -shared -lshell32 dll_enhancedbatch.c extensions.c offsets.c -o enhancedbatch_amd64.dll -Wall
+windres -F pe-x86-64 enhancedbatch.rc enhancedbatch_info_amd64.o
+gcc -Wl,-e,_dllstart -nostartfiles -O3 -s -m64 -shared -lshell32 dll_enhancedbatch.c extensions.c offsets.c enhancedbatch_info_amd64.o -o enhancedbatch_amd64.dll -Wall
+
+del *.o
