5f98971d63301e232d6cf8f122f9fd3b95537979
Jason Hood <jadoxa@yahoo.com.au>
2019-10-21 22:48:16 +1000

    Add EB.CMD to run a command using Enhanced Batch

---
+++ eb.cmd
@@ -0,0 +1,5 @@
+:: Run a command using Enhanced Batch.
+@echo off
+rundll32.exe %~dp0enhancedbatch_%processor_architecture%.dll,load
+if not defined @enhancedbatch echo Enhanced Batch didn't load.&exit /b 1
+%*
