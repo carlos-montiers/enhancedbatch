f7a794d106e07b0dfb1d502f5db1802f9c46d79d
Jason Hood <jadoxa@yahoo.com.au>
2019-10-21 21:16:22 +1000

    Extract common info for the compile commands

--- compile.cmd
+++ compile.cmd
@@ -1,10 +1,15 @@
 @Echo Off
 :: Compiler used: TDM GCC MinGW-w64 edition 5.1.0-2
 
+SetLocal
+set FLAGS=-nostartfiles -O3 -s -shared -Wall
+set LIBS=-lversion
+set SRC=dll_enhancedbatch.c extensions.c offsets.c patch.c
+
 windres -F pe-i386 -U _WIN64 enhancedbatch.rc enhancedbatch_info_x86.o
-gcc -Wl,-e,__dllstart,--enable-stdcall-fixup -nostartfiles -O3 -s -m32 -shared dll_enhancedbatch.c extensions.c offsets.c patch.c enhancedbatch_info_x86.o -lversion -o enhancedbatch_x86.dll -Wall
+gcc -Wl,-e,__dllstart,--enable-stdcall-fixup -m32 %FLAGS% %SRC% enhancedbatch_info_x86.o %LIBS% -o enhancedbatch_x86.dll
 
 windres -F pe-x86-64 enhancedbatch.rc enhancedbatch_info_amd64.o
-gcc -Wl,-e,_dllstart -nostartfiles -O3 -s -m64 -shared -lshell32 dll_enhancedbatch.c extensions.c offsets.c patch.c enhancedbatch_info_amd64.o -lversion -o enhancedbatch_amd64.dll -Wall
+gcc -Wl,-e,_dllstart -m64 %FLAGS% %SRC% enhancedbatch_info_amd64.o %LIBS% -o enhancedbatch_amd64.dll
 
 del *.o
