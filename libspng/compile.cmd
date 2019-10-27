da50a519fcda3ebb540e0b950e11628efaab914e
Carlos Montiers A <cmontiers@gmail.com>
2019-10-27 14:05:12 -0300

    Generate example.exe of 48.5 KB

--- libspng/compile.cmd
+++ libspng/compile.cmd
@@ -2,23 +2,23 @@
 pushd zlib-1.2.11
 ::mingw32-make.exe  -f win32/Makefile.gcc
 set arq=-m32
-set flags=%arq% -O3 -s -Wall -c -o
-gcc %flags% adler32.o adler32.c
-gcc %flags% compress.o compress.c
-gcc %flags% crc32.o crc32.c
-gcc %flags% deflate.o deflate.c
-gcc %flags% gzclose.o gzclose.c
-gcc %flags% gzlib.o gzlib.c
-gcc %flags% gzread.o gzread.c
-gcc %flags% gzwrite.o gzwrite.c
-gcc %flags% infback.o infback.c
-gcc %flags% inffast.o inffast.c
-gcc %flags% inflate.o inflate.c
-gcc %flags% inftrees.o inftrees.c
-gcc %flags% trees.o trees.c
-gcc %flags% uncompr.o uncompr.c
-gcc %flags% zutil.o zutil.c
+set flags=%arq% -Os -s -Wall
+set optimize=-flto -fomit-frame-pointer
+gcc %flags% -c -o adler32.o adler32.c
+gcc %flags% -c -o compress.o compress.c
+gcc %flags% -c -o crc32.o crc32.c
+gcc %flags% -c -o deflate.o deflate.c
+gcc %flags% -c -o gzclose.o gzclose.c
+gcc %flags% -c -o gzlib.o gzlib.c
+gcc %flags% -c -o gzread.o gzread.c
+gcc %flags% -c -o gzwrite.o gzwrite.c
+gcc %flags% -c -o infback.o infback.c
+gcc %flags% -c -o inffast.o inffast.c
+gcc %flags% -c -o inflate.o inflate.c
+gcc %flags% -c -o inftrees.o inftrees.c
+gcc %flags% -c -o trees.o trees.c
+gcc %flags% -c -o uncompr.o uncompr.c
+gcc %flags% -c -o zutil.o zutil.c
 ar rcs libz.a adler32.o compress.o crc32.o deflate.o gzclose.o gzlib.o gzread.o gzwrite.o infback.o inffast.o inflate.o inftrees.o trees.o uncompr.o zutil.o
 popd
-if "%arq%"=="-m32" gcc %arq% -O3 -s example.c spng.c zlib-1.2.11\libz.a -o example.exe
-if "%arq%"=="-m64" gcc %arq% -O3 -s example.c spng.c zlib-1.2.11\libz.a -o example.exe
+gcc %flags% %optimize% example.c spng.c zlib-1.2.11\libz.a -o example.exe
