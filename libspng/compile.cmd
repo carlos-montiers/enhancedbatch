67169121841e52edb58077f431049a7f1a9cd15d
Jason Hood <jadoxa@yahoo.com.au>
2019-10-28 15:27:35 +1000

    Better compile.cmd, reduce 32-bit to 47K

--- libspng/compile.cmd
+++ libspng/compile.cmd
@@ -1,24 +1,36 @@
 @echo off
-pushd zlib-1.2.11
-::mingw32-make.exe  -f win32/Makefile.gcc
-set arq=-m32
-set flags=%arq% -Os -s -Wall
+setlocal enabledelayedexpansion
+
+set ZLIBDIR=zlib-1.2.11
+
+if "%~1" == "" (
+  call :compile 32
+  call :compile 64
+) else if "%~1" == "32" (
+  call :compile 32
+) else if "%~1" == "64" (
+  call :compile 64
+) else if "%~1" == "clean" (
+  del *.o %ZLIBDIR%\*.o
+) else (
+  echo Usage: %0 [32 ^| 64 ^| clean]
+)
+goto :eof
+
+:compile
+set flags=-m%1 -Os -s -Wall
 set optimize=-flto -fomit-frame-pointer
-gcc %flags% -c -o adler32.o adler32.c
-gcc %flags% -c -o compress.o compress.c
-gcc %flags% -c -o crc32.o crc32.c
-gcc %flags% -c -o deflate.o deflate.c
-gcc %flags% -c -o gzclose.o gzclose.c
-gcc %flags% -c -o gzlib.o gzlib.c
-gcc %flags% -c -o gzread.o gzread.c
-gcc %flags% -c -o gzwrite.o gzwrite.c
-gcc %flags% -c -o infback.o infback.c
-gcc %flags% -c -o inffast.o inffast.c
-gcc %flags% -c -o inflate.o inflate.c
-gcc %flags% -c -o inftrees.o inftrees.c
-gcc %flags% -c -o trees.o trees.c
-gcc %flags% -c -o uncompr.o uncompr.c
-gcc %flags% -c -o zutil.o zutil.c
-ar rcs libz.a adler32.o compress.o crc32.o deflate.o gzclose.o gzlib.o gzread.o gzwrite.o infback.o inffast.o inflate.o inftrees.o trees.o uncompr.o zutil.o
-popd
-gcc %flags% %optimize% example.c spng.c zlib-1.2.11\libz.a -o example.exe
+set ZLIB=%ZLIBDIR%\adler32.c^
+ %ZLIBDIR%\crc32.c^
+ %ZLIBDIR%\inffast.c^
+ %ZLIBDIR%\inflate.c^
+ %ZLIBDIR%\inftrees.c^
+ %ZLIBDIR%\zutil.c
+:: Assume all or nothing.
+if not exist %ZLIBDIR%\inflate_%1.o (
+  for %%C in (%ZLIB%) do gcc %flags% %optimize% -c -o %%~pnC_%1.o %%C
+)
+if not exist spng_%1.o (
+  gcc %flags% %optimize% -c -o spng_%1.o spng.c
+)
+gcc %flags% %optimize% example.c spng_%1.o !ZLIB:.c=_%1.o! -o example_%1.exe
