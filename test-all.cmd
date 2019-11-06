1b34990c392b75f2502038dda86df22cb639749f
Jason Hood <jadoxa@yahoo.com.au>
2019-11-06 16:38:14 +1000

    Improve test-all.cmd
    Allow test-all to run on a 32-bit OS and from a 32-bit CMD.  Display the
    name of the test.  Pass any argument to suppress the pause.

--- test-all.cmd
+++ test-all.cmd
@@ -1,11 +1,22 @@
 @echo off
 
-echo Testing amd64:
-%WinDir%\System32\cmd.exe /c test1.cmd
-%WinDir%\System32\cmd.exe /c test2.cmd
+call :test %PROCESSOR_ARCHITECTURE% System32
 
-echo Testing x86:
-%WinDir%\SysWOW64\cmd.exe /c test1.cmd
-%WinDir%\SysWOW64\cmd.exe /c test2.cmd
+if defined PROCESSOR_ARCHITEW6432 (
+	call :test %PROCESSOR_ARCHITEW6432% SysNative
+) else if not %PROCESSOR_ARCHITECTURE% == x86 (
+	call :test x86 SysWOW64
+)
 
-pause
+if "%~1" == "" pause
+goto :eof
+
+:test
+echo Testing %1:
+call :runtest %2 test1.cmd 
+call :runtest %2 test2.cmd 
+goto :eof
+
+:runtest
+<nul set /p "=* %~n2: "
+%WinDir%\%1\cmd.exe /c %2
