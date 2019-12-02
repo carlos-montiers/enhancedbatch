899dd3a6ca324e2bcb7d301265c8023f97bb514f
Jason Hood <jadoxa@yahoo.com.au>
2019-12-02 11:20:37 +1000

    Have DEMO-IMAGE pass options to IMAGE

--- demo-image.cmd
+++ demo-image.cmd
@@ -6,18 +6,22 @@ If Defined WT_SESSION (
 rundll32 "%~dp0enhancedbatch_%processor_architecture%" load
 If Not Defined @enhancedbatch Goto :Eof
 
-set "demoimgfile=icon-eb.png"
+set "$demoimgfile=icon-eb.png"
 
 if %$#%==0 (
-  set "imgfile=%$0;~dp%%demoimgfile%"
-  Echo %0 image
-  Echo Using: "%demoimgfile%" as example
+  set "$imgfile=%$0;~dp%%$demoimgfile%"
+  Echo Usage: %0 image [options]
+  Echo Using "%$demoimgfile%" as example.
+) else if "%~1"=="/?" (
+  call @image /?
+  goto :eof
 ) else (
-  set "imgfile=%~1"
+  set "$imgfile=%~1"
+  set "$options=%$2-%"
 )
-call @image /q "%imgfile%"
+call @image %$options% /q "%$imgfile%"
 if %errorlevel%==0 (
-  Echo Failed to load "%imgfile%".
+  Echo Failed to load "%$imgfile%".
   Goto :Eof
 )
 :: Keep it simple for now and assume all images are the same size.
@@ -25,25 +29,40 @@ set $size=%errorlevel%
 set /a $height=%$size% ^>^> 8 ^& 0xFF
 set /a $width=%$size% ^& 0xFF
 
-:: Make room for the image.
-for /l %%L in (2,1,%$height%) do echo.
-set /a $r=%@row% - (%$height% - 1)
-set @row=%$r%
-
-set $cursor=@cursor
-set @cursor=off
 set $delayedexpansion=@delayedexpansion
 set @delayedexpansion=on
 
-call @image /copy /n "%imgfile%"
-set $frames=%errorlevel%
+for /l %%O in (2,1,%$#%) do (
+  if /i "!$%%O!"=="/P" (
+    set $r=1
+  ) else if /i "!$%%O!"=="/R" (
+    set $r=1
+  ) else if /i "!$%%O!"=="/RH" (
+    set $r=1
+  ) else if /i "!$%%O!"=="/F" (
+    set $frames=1
+  )
+)
+
+:: Make room for the image (unless row was given).
+if not defined $r (
+  for /l %%L in (2,1,%$height%) do echo.
+  set /a $r=!@row! - (%$height% - 1^)
+  set @row=!$r!
+)
+
+set $cursor=@cursor
+set @cursor=off
+
+call @image %$options% /copy /n "%$imgfile%"
+if not defined $frames set $frames=%errorlevel%
 if %$frames%==1 (
-  call @image "%imgfile%"
+  call @image %$options% "%$imgfile%"
   call @getkb
 ) else (
   set $f=0
   for do (
-    call @image /f !$f! "%imgfile%"
+    call @image %$options% /f !$f! "%$imgfile%"
     set /a $delay=!errorlevel! ^>^> 16
     if !$delay!==0 set $delay=100
     call @sleep !$delay!
@@ -51,7 +70,7 @@ if %$frames%==1 (
     call @kbhit || set @next=
   )
 )
-call @image /restore "%imgfile%"
+call @image %$options% /restore "%$imgfile%"
 
 set @delayedexpansion=$delayedexpansion
 set @cursor=$cursor
