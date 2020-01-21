ffddbdfaa832acc1d0161c25b61ae0c95aa46a13
Jason Hood <jadoxa@yahoo.com.au>
2020-01-21 18:18:54 +1000

    Enhance eb.cmd
    Allow eb.cmd to load EB into the command line (thanks to a trick from
    Dave), unload it from the command line, or load it into a batch (with a
    failure message and exit code).

--- eb.cmd
+++ eb.cmd
@@ -1,5 +1,29 @@
-:: Run a command using Enhanced Batch.
+:: Run a command using Enhanced Batch, or load/unload it.
 @echo off
-rundll32.exe "%~dp0enhancedbatch_%processor_architecture%.dll" load
-if not defined @enhancedbatch echo Enhanced Batch didn't load.&exit /b 1
+
+:: No arguments, load into a batch file.
+if "%~1"=="" (
+  if not defined @enhancedbatch rundll32.exe "%~dp0enhancedbatch_%processor_architecture%.dll" load
+  if not defined @enhancedbatch >&2 echo Enhanced Batch failed to load.&exit /b 1
+  goto :eof
+)
+
+:: Load into the command line (or a batch, if you don't want the above message,
+:: but you won't get the exit code, either).
+if "%~1"=="load" (
+  if defined @enhancedbatch goto :eof
+  2>nul goto&rundll32.exe "%~dp0enhancedbatch_%processor_architecture%.dll" load
+)
+
+:: Unload from the command line (DON'T call from a batch).
+if "%~1"=="unload" (
+  if not defined @enhancedbatch goto :eof
+  :: Not really necessary, but it's nice symmetry.
+  2>nul goto&call @unload
+)
+
+if not defined @enhancedbatch (
+  rundll32.exe "%~dp0enhancedbatch_%processor_architecture%.dll" load
+  if not defined @enhancedbatch exit /b 1
+)
 call %*
